290068aa40e2f24356bff764f314ba90b0a24830
ONIER_过的数字 <2782180957@qq.com>
2020-12-07 14:08:58 +0800

    登录-KONG

---
+++ vueapp/src/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        LOGIN
+        <el-button>button</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+</style>

a46bc6788b101892fe387f5a78ab0422cb21959a
ONIER_过的数字 <2782180957@qq.com>
2020-12-09 15:59:13 +0800

    HOME

---
+++ application/service/src/main/java/com/gp/service/impl/MenuServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gp.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.gp.dao.MenuDao;
+import com.gp.service.MenuService;
+import com.gp.vo.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuServiceImpl implements MenuService {
+
+    @Autowired
+    MenuDao menuDao;
+
+    @Override
+    public List<Menu> getMenu(int id) {
+        Menu menu = new Menu();
+        menu.setPrint(id);
+        return menuDao.selectList(new QueryWrapper<Menu>(menu));
+    }
+}

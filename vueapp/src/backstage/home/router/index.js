9946a2230ca27403269d02251a6d8c91a90514fb
ONIER_过的数字 <2782180957@qq.com>
2020-12-09 16:47:38 +0800

    路由

--- vueapp/src/backstage/home/router/index.js
+++ vueapp/src/backstage/home/router/index.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import staffManagement from "@/backstage/home/view/staffManagement.vue";
+
 Vue.use(VueRouter);
 
 let routes = [
+    {
+        path:"/staffManagement",
+        name: "staffManagement",
+        component: staffManagement,
+    }
 ];
 
 export default new VueRouter({
